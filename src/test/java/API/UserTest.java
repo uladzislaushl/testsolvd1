21f514f58b0aadf66666ce99c11c2436476345f5
Vladislav <vlad.shlovenets19@gmail.com>
2024-03-21 15:36:51 +0300

    TestJava

---
+++ src/test/java/API/UserTest.java
@@ -0,0 +1,25 @@
+package API;
+
+import org.junit.Test;
+import io.restassured.http.ContentType;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+
+public class UserTest {
+
+    private final static String Link = "https://reqres.in/";
+
+    @Test
+    public void checkIDAvatar(){
+        List<UserData> users = given()
+                .when()
+                .contentType(ContentType.JSON)
+                .get(Link +"/api/users?page=2")
+                .then().log().all()
+                .extract().body().jsonPath().getList("data", UserData.class);
+
+        int i = 0;
+    }
+}
